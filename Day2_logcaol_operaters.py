47d3e04a0d9ce79c9b127a8f7c24169bdeb0c521
shoki-kubota-jp <shoki_kubota@niandc.co.jp>
2024-11-05 10:14:45 +0530

    logcal and conparition operators

---
+++ Day2_logcaol_operaters.py
@@ -0,0 +1,12 @@
+# Equal
+print(5 == 5)
+# Not Equal
+print(5 != 3)
+# Greater Than
+print(5 > 3)
+# Less Than
+print(3 < 5)
+# Greater Than or Equal to
+print(5 >= 5)
+# Less Than or Equal to
+print(3 <= 5)
